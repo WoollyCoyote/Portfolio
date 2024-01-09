f77820e746023d67bb6e44a4453dc7498be7682a
Cody O'Kelly <pfcokelly@gmail.com>
2024-01-09 16:30:57 -0500

    nextcrud

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,8 +1,9 @@
 "use client";
 import { useState, useEffect } from "react";
+import { FaGithubAlt } from "react-icons/fa";
+import Image from "next/image";
+import Link from "next/link";
 import "./portfolio.css";
-import { BsCaretUpFill } from "react-icons/bs"
-import PortfolioEle from "./portfolioEle";
 import Weather from "./projects/weather";
 
 const Portfolio = () => {
@@ -29,49 +30,88 @@ const Portfolio = () => {
 
   return (
     <div>
-        <div className="card">
-          {pageDetails.getWeather && (
-            <Weather
-              weatherLocation={pageDetails.weatherLocation.toUpperCase()}
-            />
-          )}
-          <input
-            id="textInput"
-            hidden={pageDetails.inputVis}
-            type="text"
-            placeholder="City, town, zip"
-            className="input"
-            onChange={(event) => {
-              setPageDetails({
-                ...pageDetails,
-                weatherLocation: event.target.value,
-              });
-            }}
+
+      <div className="card oilAppBox">
+        <h2 className="title">Oil Life</h2>
+        <h4 className="subTitle">
+          NEXT CRUD APP with a seperate JSON server for backend Data
+        </h4>
+        <div className="oilImgandDis">
+
+        <Image
+        className="oilAppImg"
+        src="/oillifescreenshotv1.png"
+        width={500}
+        height={500}
+        alt="Project screen shot"
+        />
+        <p className="oilAppDiscription">
+          a oil tracking app for keeping a log of each of youre vehicles current oil use and when to change oil. The Data is
+          stored with a Json server running on *Render.com* and the front end is deployed on vercel
+        </p>
+        <div className="Oillinks">
+          <div className="oilLinkLive">
+            <Link href="https://nextcrud-sage.vercel.app">Deployed on</Link>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 353 64"
+              >
+              <path
+                fill="white"
+                d="M141 16c-11 0-19 7-19 18s9 18 20 18c7 0 13-3 16-7l-7-5c-2 3-6 4-9 4-5 0-9-3-10-7h28v-3c0-11-8-18-19-18zm-9 15c1-4 4-7 9-7s8 3 9 7h-18zm117-15c-11 0-19 7-19 18s9 18 20 18c6 0 12-3 16-7l-8-5c-2 3-5 4-8 4-5 0-9-3-11-7h28l1-3c0-11-8-18-19-18zm-10 15c2-4 5-7 10-7s8 3 9 7h-19zm-39 3c0 6 4 10 10 10 4 0 7-2 9-5l8 5c-3 5-9 8-17 8-11 0-19-7-19-18s8-18 19-18c8 0 14 3 17 8l-8 5c-2-3-5-5-9-5-6 0-10 4-10 10zm83-29v46h-9V5h9zM37 0l37 64H0L37 0zm92 5-27 48L74 5h10l18 30 17-30h10zm59 12v10l-3-1c-6 0-10 4-10 10v15h-9V17h9v9c0-5 6-9 13-9z"
+                />
+            </svg>
+          </div>
+          <div id="githubLink" className="oilLinkGit">
+            <FaGithubAlt />
+            <Link href="https://github.com/WoollyCoyote/nextcrud.git">
+              Github
+            </Link>
+          </div>
+                </div>
+        </div>
+      </div>
+
+
+      <div className="card weatherBox">
+      <h2 className="title">Weather app</h2>
+        <h3 className="subTitle">API app rebuilt as a React Component that changes the parallax wallpaper dpending on youre weather condition</h3>
+        {pageDetails.getWeather && (
+          <Weather
+            weatherLocation={pageDetails.weatherLocation.toUpperCase()}
           />
-          <button
-            onClick={() => {
-              handleBtnClick();
-            }}
-            id="clickToEnter"
-            className="wthrbtn"
+        )}
+
+       <div className="weatherInputBox">
+        <input
+          id="textInput"
+          hidden={pageDetails.inputVis}
+          type="text"
+          placeholder="City, town, zip"
+          className="weatherInput"
+          onChange={(event) => {
+            setPageDetails({
+              ...pageDetails,
+              weatherLocation: event.target.value,
+            });
+          }}
+        />
+        <button
+          onClick={() => {
+            handleBtnClick();
+          }}
+          id="clickToEnter"
+          className="wthrbtn"
           >
-            {pageDetails.btn}
-          </button>
-          {!pageDetails.getWeather && (
-            <div>
-
-            <BsCaretUpFill />
-            <p className="fw-normal">
-  enter your town or city to get the weather
-</p>
-
-            </div>
-)}
+          {pageDetails.btn}
+        </button>
         </div>
-
-        <div className={"project"}>
-          <PortfolioEle />
-        </div>
+        {!pageDetails.getWeather && (
+          <div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/portfolio/projects/weather.js
+++ src/components/portfolio/projects/weather.js
@@ -1,8 +1,7 @@
-import "./weather.css";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 const Weather = (props) => {
-  let test = false;
+  let test = true;
   let fake =
     '{"location":{"name":"Flint","region":"Michigan","country":"United States of America","lat":43.01,"lon":-83.69,"tz_id":"America/Detroit","localtime_epoch":1687637380,"localtime":"2023-06-24 16:09"},"current":{"last_updated_epoch":1687636800,"last_updated":"2023-06-24 16:00","temp_c":27.8,"temp_f":82.0,"is_day":1,"condition":{"text":"Partly cloudy","icon":"//cdn.weatherapi.com/weather/64x64/day/116.png","code":1003},"wind_mph":4.3,"wind_kph":6.8,"wind_degree":320,"wind_dir":"NW","pressure_mb":1012.0,"pressure_in":29.89,"precip_mm":0.0,"precip_in":0.0,"humidity":56,"cloud":25,"feelslike_c":28.3,"feelslike_f":83.0,"vis_km":16.0,"vis_miles":9.0,"uv":8.0,"gust_mph":5.4,"gust_kph":8.6}}';
   let [weatherapi, setWeatherapi] = useState();
@@ -28,7 +27,8 @@ const Weather = (props) => {
       const results = await response.text();
       setWeatherapi(JSON.parse(results));
       setLoaded(!loaded);
-      console.log(results, locationToUpperCase);
+      // console.log(results, locationToUpperCase);
+      console.log(weatherapi.current.condition.icon)
     } catch (error) {
       console.error(error);
     }
@@ -44,14 +44,16 @@ const Weather = (props) => {
     <div>
       <div id="weatherAPI">
         {loaded && (
-          <div>
+          <div className="weatherData">
             <h1 className="weatherLocation">{props.weatherLocation}</h1>
             <p>{weatherapi.current.temp_f}</p>
             <p>{weatherapi.current.condition.text}</p>
-            <Image width="auto"
-          height="auto"
-          
-          alt="a rain cloud, sun or other weather images" url={weatherapi.current.condition.icon}/>
+            <Image
+              width={64}
+              height={64}
+              alt="a rain cloud, sun or other weather images"
+              src={`https:${weatherapi.current.condition.icon}`}
+            />
           </div>
         )}
         {!loaded && (
@@ -62,12 +64,9 @@ const Weather = (props) => {
         )}
       </div>
       <div id="weatherBox">
-        <h2 id="weatherTitle" className="weatherTitle"></h2>
-        <Image
-          id="pic"
-          visability="hidden"
-   
-        />
+
+        {/* <h2 id="weatherTitle" className="weatherTitle"></h2> */}
+        {/* <Image id="pic" visability="hidden" /> */}
       </div>
     </div>
   );

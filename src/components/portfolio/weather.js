2b3febceaa920e623ebf0b2e995e8d3da5151fcf
Cody O'Kelly <pfcokelly@gmail.com>
2024-01-10 20:30:40 -0500

    version 3.12 fixed the weather css for when there is data locking it

--- src/components/portfolio/weather.js
+++ src/components/portfolio/weather.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 const Weather = (props) => {
-  let test = true;
+  let test = false;
   let fake =
     '{"location":{"name":"Flint","region":"Michigan","country":"United States of America","lat":43.01,"lon":-83.69,"tz_id":"America/Detroit","localtime_epoch":1687637380,"localtime":"2023-06-24 16:09"},"current":{"last_updated_epoch":1687636800,"last_updated":"2023-06-24 16:00","temp_c":27.8,"temp_f":82.0,"is_day":1,"condition":{"text":"Partly cloudy","icon":"//cdn.weatherapi.com/weather/64x64/day/116.png","code":1003},"wind_mph":4.3,"wind_kph":6.8,"wind_degree":320,"wind_dir":"NW","pressure_mb":1012.0,"pressure_in":29.89,"precip_mm":0.0,"precip_in":0.0,"humidity":56,"cloud":25,"feelslike_c":28.3,"feelslike_f":83.0,"vis_km":16.0,"vis_miles":9.0,"uv":8.0,"gust_mph":5.4,"gust_kph":8.6}}';
   let [weatherapi, setWeatherapi] = useState();
